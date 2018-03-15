6386b31d5aae39ad2aafd1b78bfed5ac96aea494
likun <lklikun123@163.com>
2018-03-15 15:38:57 +0800

    新建service

---
+++ dubbo-demo-interface/src/main/java/com/lk/dubbo/demo/service/GreetingService.java
@@ -0,0 +1,10 @@
+package com.lk.dubbo.demo.service;
+
+/**
+ * @author likun
+ * @version V1.0
+ * @Title: com.lk.dubbo.demo.service
+ * @date 2018/3/15 15:37
+ */
+public class GreetingService {
+}
